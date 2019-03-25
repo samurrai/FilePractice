30fe333acece534bbc421eb50f1350e75eda6fff
samurrai <samurrrai.r@gmail.com>
2019-03-26 02:29:47 +0600

    Практика

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Symbol> symbols = new List<Symbol>();
+            for (int i = 0; i < 256; i++)
+            {
+                Symbol tmp = new Symbol() {
+                    Value = (char)i,
+                    Counter = 0
+                };
+                symbols.Add(tmp);
+            }
+            using (FileStream reader = new FileStream("data.txt", FileMode.Open))
+            {
+                for (int i = 0; i < reader.Length; i++)
+                {
+                    int current = reader.ReadByte();
+                    for (int j = 0; j < symbols.Count; j++)
+                    {
+                        if (symbols[j].Value == current)
+                        {
+                            symbols[j].Counter++;
+                            break;
+                        }
+                    }
+                }
+            }
+            foreach (var symbol in symbols)
+            {
+                if (symbol.Counter > 0)
+                {
+                    Console.WriteLine($"{symbol.Value} - {symbol.Counter}");
+                }
+            }
+            
+            using (StreamWriter streamWriter = new StreamWriter("text.txt"))
+            {
+                streamWriter.WriteLine("Руслан");
+                streamWriter.WriteLine("Шадикьян");
+                streamWriter.WriteLine("15");
+            }
+        }
+    }
+}
---
+++ Symbol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilePractice
+{
+    public class Symbol
+    {
+        public char Value { get; set; }
+        public int Counter { get; set; }
+    }
+}
